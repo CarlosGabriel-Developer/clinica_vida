b0933cf4c188c0ec9aee605f2c12aad5a4f9cf3f
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-11 19:31:36 -0300

    add

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ paciente = {}
 
 print ("-"*40)
 
+print("=== SISTEMA CLÍNICA VIDA+ ===")
+print('[1] CADASTRAR PACIENTE')
+print('[2] VER ESTATÍSTICAS')
+print('[3] BUSCAR PACIENTE')
+print('[4] LISTA DE TODOS OS PACIENTES')
+print('[5] SAIR')
+
+print ("-"*40)
+
+usuario_escolha = int(input('ESCOLHA UMA OPÇÃO: '))
+
 while True : 
     
     cadastrar_paciente = str(input("Deseja continuar [S/N]: "))

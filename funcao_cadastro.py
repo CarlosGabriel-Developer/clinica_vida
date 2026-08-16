9447cb25d2060cb690bd9e87a552deac7b5a7074
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-16 13:41:24 -0300

    add

--- funcao_cadastro.py
+++ funcao_cadastro.py
@@ -1,37 +1,38 @@
-from datetime import datetime
+def cadastro_paciente(nome,telefone,ano_nascimento) :
     
-banco_dados_pacientes = []
-
-
-
-usuario_escolha = str(input('ESCOLHA UMA OPÇÃO ENTRE 1 - 5 : '))
-
-if usuario_escolha == "1" : 
-    
+    banco_dados_pacientes = [], 
     paciente = {}
     
-    print('=== CADASTRAR PACIENTE ===')
+    from datetime import datetime
+    ano_atual = datetime.now().year
     
-    paciente['Nome'] = str(input('NOME DO PACIENTE: ')).upper()
-    
+    nome = str(input('Nome do paciente: ')).capitalize()
+    paciente['Nome'] = nome
+
     while True : 
-    
-        paciente_ano_nacimento = int(input('ANO DE NACIMENTO DO PACIENTE: '))
+
+        ano_nascimento = int(input('Ano de nacimento do paciente: '))
         
-        idade = paciente_ano_nacimento - datetime.today().year
+        idade_atual_paciente = ano_atual - ano_nascimento
         
-        if 0 <= idade <= 120 : 
-            paciente['Idade'] = idade
+        if 0 <=idade_atual_paciente <= 120 : 
+            paciente['Idade'] = idade_atual_paciente
             break
         
         else : 
             print('ANO DE NASCIMENTO INVÁLIDO')
+            
+    telefone = input('Digite o telefone apenas com números: ')
+
+    if telefone.isdigit() and len(telefone) == 11:
+        print('Telefone válido')
+        paciente['Telefone'] = telefone
         
-    paciente['Telefone'] = str(input('TELEFONE (DDD) XXXXX-XXXX: '))
-        
+    else:
+        print('Telefone inválido')
+            
     banco_dados_pacientes.append(paciente)
-    
+        
     print('PACIENTE CADASTRADO COM SUCESSO')
-    
     
     
--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-
-ano_atual = datetime.today().year
-
-banco_dados_pacientes = []
 
 print ("-"*40)
 
@@ -15,59 +10,5 @@ print('[5] SAIR')
 
 print ("-"*40)
 
-usuario_escolha = str(input('ESCOLHA UMA OPÇÃO ENTRE 1 - 5 : '))
 
-if usuario_escolha == "1" : 
-    
-    paciente = {}
-
-    print('=== CADASTRAR PACIENTE ===')
-    print ("-"*40)
-
-    paciente['Nome'] = str(input('NOME DO PACIENTE: ')).upper()
-    
-    while True : 
-    
-        paciente_ano_nacimento = int(input('ANO DE NACIMENTO DO PACIENTE: '))
-        
-        idade = datetime.today().year - paciente_ano_nacimento
-        
-        if 0 <= idade <= 120 : 
-            paciente['Idade'] = idade
-            break
-        
-        else : 
-            print('ANO DE NASCIMENTO INVÁLIDO')
-        
-    paciente['Telefone'] = str(input('TELEFONE (DDD) XXXXX-XXXX: '))
-        
-    banco_dados_pacientes.append(paciente)
-    
-    print('PACIENTE CADASTRADO COM SUCESSO')
-    
-    print ("-"*40)
-    
-elif usuario_escolha == '2':
-    
-    numero_pacientes_cadastrados = 0
-    paciente_mais_novo = None
-    paciente_mais_velho = None
-
-for paciente in banco_dados_pacientes:
-    numero_pacientes_cadastrados += 1
-
-    if paciente_mais_velho is None or paciente['Idade'] > paciente_mais_velho:
-        paciente_mais_velho = paciente['Idade']
-
-    if paciente_mais_novo is None or paciente['Idade'] < paciente_mais_novo:
-        paciente_mais_novo = paciente['Idade']
-    
-while True : 
-    
-    cadastrar_paciente = str(input("Deseja continuar [S/N]: "))
-    
-    if cadastrar_paciente not in "Ss" :
-        print("Fim do Programa") 
-        break
-    
 print ("-"*40)
